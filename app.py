b55b4b99abb20a91f6f4b91f44e58f9806dc5922
Pawan Lamba <pawan88.lamba@gmail.com>
2020-06-02 02:05:28 -0400

    Organizing the code by creating functions

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 
-wb = xl.load_workbook('transactions.xlsx')
-sheet = wb['Sheet1']
-cell = sheet['a1']
-# print(cell.value)
+def process_workbook(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
 
-# Add new discounted value in new column
-for row in range(2, sheet.max_row + 1):
-    cell = sheet.cell(row, 3)
-    corrected_price = cell.value * 0.9
-    corrected_price_cell = sheet.cell(row, 4)
-    corrected_price_cell.value = corrected_price
+    # Add new discounted value in new column
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
 
-# Bar Chart
-values = Reference(sheet,
+    # add bar chart
+    values = Reference(sheet,
               min_row=2,
               max_row=sheet.max_row,
               min_col=4,
               max_col=4)
 
-chart = BarChart()
-chart.add_data(values)
-sheet.add_chart(chart, 'e2')
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
 
-wb.save('transactions_updated.xlsx')
+    wb.save(filename)
+
+
+process_workbook('transactions.xlsx')
